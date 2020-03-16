188218415755f6b2457f6677b00273191e8de313
SaadBadr <saadbadr23@gmail.com>
2020-03-16 16:17:54 +0200

    modifying test.py

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import HuffmanCodes as code
 
-codes = code.saad_please_i_need_the_codes('aaaaabbbbbbbbbccccccccccccdddddddddddddeeeeeeeeeeeeeeeefffffffffffffffffffffffffffffffffffffffffffff')
+(dictionary, root) = code.saad_please_i_need_the_codes('aaaaabbbbbbbbbccccccccccccdddddddddddddeeeeeeeeeeeeeeeefffffffffffffffffffffffffffffffffffffffffffff')
 
-print(codes)
+# the function return the codes in a dictionary and the root as a node object
+
+
+print(dictionary)
+
+print(root.frequency)
+
